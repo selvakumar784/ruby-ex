6d9de465389ba9827196e7d25286a6508a33ae76
Selvakumar <selvakumarn@freshdesk.com>
2016-05-12 12:20:32 +0530

    Refactor code

--- businessHours.rb
+++ businessHours.rb
@@ -1,40 +1,40 @@
 require 'time'
 require 'date'
 
+DAYS = { "mon" => 0, "tue" => 1,
+         "wed" => 2, "thu" => 3,
+         "fri" => 4, "sat" => 5,
+         "sun" => 6
+       } 
+
 class BusinessHours
-  @@days = {"sun" => 6, "mon" => 0,
-            "tue" => 1, "wed" => 2,
-            "thu" => 3, "fri" => 4,
-            "sat" => 5}
             
-  @@h = Hash.new{|hsh, key| hsh[key] = []}
+  @@businessHash = Hash.new{|hsh, key| hsh[key] = []}
   
-  def initialize(startTime, endTime, cur_date=Time.new.localtime.to_date)
-    @startTime = startTime
-    @endTime = endTime
-    @cur_date = cur_date.to_s
+  def initialize(start_time, end_time, cur_date=Time.new.localtime.to_date)
+    cur_date = cur_date.to_s
 
     #Check if given date exists in the past.
     #If its a valid date, add an entry if the date doesn't exist already.
-    temp_date = Date.parse @cur_date
+    temp_date = Date.parse cur_date
     if temp_date < Time.new.localtime.to_date
       abort("The date is in past. Provide a valid entry.")
     else
-      if @@h.has_key? cur_date
+      if @@businessHash.has_key? cur_date
         abort("Entry exists already. You can only update.")
       else
-        @@h[@cur_date].push @startTime
-        @@h[@cur_date].push @endTime
+        @@businessHash[cur_date].push start_time
+        @@businessHash[cur_date].push end_time
       end
     end
   end
   
-  def update(day_or_date, newStartTime, newEndTime)
+  def update(day_or_date, new_start_time, new_end_time)
     day_or_date = day_or_date.to_s
     
     #Convert given day to date
-    if @@days.has_key? day_or_date
-      day_id = @@days[day_or_date]
+    if DAYS.has_key? day_or_date
+      day_id = DAYS[day_or_date]
       datey = Date.today
       conv_date = (datey - datey.wday + day_id + 1).strftime
     else
@@ -44,12 +44,12 @@ class BusinessHours
     conv_date = conv_date.to_s
     
     #Update the entries, based on the converted date.
-    if @@h.length == 0
+    if @@businessHash.length == 0
       abort("No entry exists. Please add an entry and update it.")
     else
-      if @@h.has_key? conv_date
-        @@h[conv_date][0] = newStartTime
-        @@h[conv_date][1] = newEndTime
+      if @@businessHash.has_key? conv_date
+        @@businessHash[conv_date][0] = new_start_time
+        @@businessHash[conv_date][1] = new_end_time
       else
         abort("Illegal entry to update. Given date/day doesnt exists. Please add.")
       end
@@ -60,8 +60,8 @@ class BusinessHours
     day_or_date = day_or_date.to_s
     
     #Convert day to date.
-    if @@days.has_key? day_or_date
-      day_id = @@days[day_or_date]
+    if DAYS.has_key? day_or_date
+      day_id = DAYS[day_or_date]
       datey = Date.today
       conv_date = (datey - datey.wday + day_id + 1).strftime
     else
@@ -71,35 +71,34 @@ class BusinessHours
     conv_date = conv_date.to_s
 
     #Add Opening time and closing time as 0:00 for a given closed date.
-    if @@h.has_key? conv_date
-      @@h[conv_date][0] = "0:00"
-      @@h[conv_date][1] = "0:00"
+    if @@businessHash.has_key? conv_date
+      @@businessHash[conv_date][0] = "0:00"
+      @@businessHash[conv_date][1] = "0:00"
     else
-      @@h[conv_date].push "0:00"
-      @@h[conv_date].push "0:00"
+      @@businessHash[conv_date].push "0:00"
+      @@businessHash[conv_date].push "0:00"
     end
   end
   
-  def calculate_deadline(timeInterval, startingTime)
-    numSeconds = timeInterval
-    startDate = startingTime.split(' ')[0]
-    startTime = startingTime.split(' ')[1] + startingTime.split(' ')[2]
+  def calculate_deadline(time_interval, submit_req)
+    num_seconds = time_interval
+    submit_date = submit_req.split(' ')[0]
+    submit_time = submit_req.split(' ')[1] + submit_req.split(' ')[2]
     
     #Sort the hash based on key
-    @@h = Hash[@@h.sort_by{|k,v| k}] 
+    @@businessHash = Hash[@@businessHash.sort_by{|k,v| k}] 
     
-    total_length = 0
-    total_length = @@h.length      
+    total_length = @@businessHash.length      
     
-    openingTime = ""
-    closingTime = ""
+    opening_time = ""
+    closing_time = ""
     cur_index = 0
     
     #Get the opening and closing time for a given request.
-    @@h.each_with_index do |(key,value), index|
-      if key == startDate
-        openingTime = value[0]
-        closingTime = value[1]
+    @@businessHash.each_with_index do |(key,value), index|
+      if key == submit_date
+        opening_time = value[0]
+        closing_time = value[1]
         cur_index = index
       end
     end    
@@ -107,62 +106,66 @@ class BusinessHours
     while cur_index < total_length
       
       #Convert the string to time objects
-      start_str = "#{startDate} #{openingTime.split(" ")[0] + openingTime.split(" ")[1]}"  
-      time_start = Time.parse(start_str)
+      start_str = "#{submit_date} #{opening_time.split(" ")[0] + opening_time.split(" ")[1]}"  
+      time_open = Time.parse(start_str)
       
-      req_str = "#{startDate} #{startTime}"
+      req_str = "#{submit_date} #{submit_time}"
       time_req = Time.parse(req_str)
       
-      end_str = "#{startDate} #{closingTime.split(" ")[0] + closingTime.split(" ")[1]}"  
-      time_end = Time.parse(end_str)
+      end_str = "#{submit_date} #{closing_time.split(" ")[0] + closing_time.split(" ")[1]}"  
+      time_close = Time.parse(end_str)
       
       
-      if time_start <= time_req
-        del_time = time_req + numSeconds
-        if  del_time <= time_end
-          puts del_time.strftime("%a %b %d %H:%M:%S %Y")
+      #Check if a request can be processed in the same day
+      if time_open <= time_req
+        delivery_time = time_req + num_seconds
+        if  delivery_time <= time_close
+          puts delivery_time.strftime("%a %b %d %H:%M:%S %Y")
           break
         else
-          diff_time = del_time - time_end
-          openingTime = ""
-          closingTime = ""
+          #Caculate remaining time
+          diff_time = delivery_time - time_close
+          opening_time = ""
+          closing_time = ""
           flag = 0
-          @@h.each_with_index do |(key, value), index|
+          #Find the next working day
+          @@businessHash.each_with_index do |(key, value), index|
             if index > cur_index
-              openingTime = value[0]
-              closingTime = value[1]
-              if openingTime != "0:00" && closingTime != "0:00"
-                startDate = key
+              opening_time = value[0]
+              closing_time = value[1]
+              if opening_time != "0:00" && closing_time != "0:00"
+                #Update the date and working hours
+                submit_date = key
                 flag = 1
                 cur_index = index
-                startTime = openingTime
-                numSeconds = diff_time
+                submit_time = opening_time
+                num_seconds = diff_time
                 break
               end
             end
-            if cur_index == @@h.length - 1
+            if cur_index == @@businessHash.length - 1
               puts "Can't schedule."
               break
             end
           end
         end
+      #Update the index to start for the next iteration,
+      #if the request is not processed on the current day.
       cur_index = cur_index + 1 if flag == 0
     end
     end
   end
 end
 
-
 #Object creation and method calls
-obj = BusinessHours.new("9:00 AM", "5:00 PM" , "2016-05-11")
-obj.update(:wed, "10:00 AM", "4:00 PM")
-obj.update("2016-05-11", "10:00 AM", "5:00 PM")
-obj.closed("2016-05-13")
-obj.closed("sat")
+obj = BusinessHours.new("9:00 AM", "5:00 PM" , "2016-05-12")
+obj.update(:thu, "10:00 AM", "4:00 PM")
+obj.update("2016-05-12", "10:00 AM", "5:00 PM")
+obj.closed("2016-05-14")
+obj.closed("sun")
 
-BusinessHours.new("9:00 AM", "05:00 PM", "2016-05-12")
-BusinessHours.new("9:00 AM", "10:00 AM", "2016-05-15")
+BusinessHours.new("9:00 AM", "05:00 PM", "2016-05-13")
 BusinessHours.new("9:00 AM", "10:00 AM", "2016-05-16")
+BusinessHours.new("9:00 AM", "10:00 AM", "2016-05-17")
 
-
-obj.calculate_deadline(2 * 60 * 60, "2016-05-12 4:30 PM")
+obj.calculate_deadline(2 * 60 * 60, "2016-05-13 4:30 PM")
