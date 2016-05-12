33a81f52b4b879c9449af54de6812f2a027fd33b
Selvakumar <selvakumarn@freshdesk.com>
2016-05-12 12:20:10 +0530

    Refactor code

--- jukebox.rb
+++ jukebox.rb
@@ -15,23 +15,14 @@ def skip_tracks(arr_cpy, i)
     count = count + 1
     start = start - 1
   end
-  return arr_cpy
+  arr_cpy
 end
 
 def skip_tracks_ruby(arr_cpy, n)
-  return arr_cpy.rotate(n)
+  arr_cpy.rotate(n)
 end
 
 arr = ["Gilly", "Pokkiri", "Thuppakki", "Kaththi", "Theri"]
-
-=begin
-res_teq = skip_tracks(arr, 1)
-res_teq1 = skip_tracks(arr, -1)
-puts res_teq
-puts "\n"
-puts res_teq1
-puts "\n"
-=end
 
 res_ruby = skip_tracks_ruby(arr, 2)
 res_ruby1 = skip_tracks_ruby(arr, -2)
